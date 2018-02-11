6400f7b720a79412caf6bd493cf71268d0d9692a
e-avery <35651443+e-avery@users.noreply.github.com>
2018-02-11 00:51:07 -0600

    State version release.
    v1.0.0

--- HFB.py
+++ HFB.py
@@ -1,61 +1,75 @@
-# High_Five_Bot
+# High_Five_Bot v1.0.0
 # Written in PRAW (Python Reddit API Wrapper)
 # Python v3.6
 
-import praw
+
+
+import os
 import time
+import praw
 from datetime import datetime
 from random import randint
 
 # Create instance, identifying user agent
 reddit = praw.Reddit(client_id='',
-		     client_secret='',
-	             password='',
-		     user_agent='High-Five Bot v0.1, by u/WadeEffingWilson',
-		     username='High_Five_Bot')
+					 client_secret='',
+					 password='',
+					 user_agent='',
+					 username='')
 
 # Feedback on login status
 if reddit.user.me() == "High_Five_Bot":
-	print("Logged in and ready to go.")
+	print("I am High_Five_Bot. Logged in and ready to go.")
 
 # set up for r/bottesting--change to r/all after completion
-subreddit = reddit.subreddit('bottesting')
+subreddit = reddit.subreddit('all')
 
 # Array of high five GIFs
-hf_gifs = ["https://i.imgur.com/NAkvu0L.gifv", "https://i.imgur.com/TITkVpi.gifv", "https://i.imgur.com/SsZJSyy.gifv"]
+hf_gifs = ["https://i.imgur.com/NAkvu0L.gifv", "https://i.imgur.com/TITkVpi.gifv", "https://i.imgur.com/SsZJSyy.gifv", "https://i.imgur.com/W3LGywp.gifv", 
+"https://i.imgur.com/TX2oxf3.gifv", "https://i.imgur.com/JpDHdLd.gifv", "https://i.imgur.com/lGpQ0ak.gifv",
+ "https://i.imgur.com/P70igZx.gifv", "https://i.imgur.com/XiDPAlW.gifv", "https://i.imgur.com/PXA8cVT.gifv"]
 
 # String attachment for ID
-botstat = ("*****"
-"I am a bot. When I'm called with !high-five or !highfive, I return a sweet high-five GIF. Call me up if you ever need to high-five someone.")
+botstat = ("""
+*****
+^("I am a good bot. When I'm called with )!high-five^( or )!highfive,^( I return a sweet high-five GIF. Call me up if you ever need to high-five someone.")
+""")
 
-# Error catch variable for troubleshooting
-errors = []
+# Check to see if already responded
+def resp_check(var):
+	with open(os.environ['USERPROFILE']+'/Documents/GitHub/HFBot/IDs.txt') as nfile:
+		for lines in nfile.readlines():
+			if str(var) in lines:
+				return 1
+			else:
+				f = 0
+	return f
 
 def crawl():
+	print("Searching...crawling...looking...")
 	for comment in subreddit.stream.comments():
-		if (comment.body.lower() == "!highfive") or (comment.body.lower() == "!high-five") and (comment.author.name != 'High_Five_Bot'):
+		if ((comment.body.lower() == "!highfive") or (comment.body.lower() == "!high-five")) and (comment.author.name != 'High_Five_Bot') and (resp_check(comment) == 0):
 			# reply to parent comment where the second argument is the UL for the array index
 			reply = "%s high-fived %s" % (comment.author, comment.parent().author)
 			comment.reply("[" + reply + "](" + hf_gifs[randint(0,2)] + ")" + botstat)
+			with open(os.environ['USERPROFILE']+'/Documents/GitHub/HFBot/IDs.txt', 'a') as file:
+				file.write(str(comment) + "\n")
+			print("Found a call.")
 			
-# Add conditional __main__ statement before here
-<<<<<<< HEAD
-if __name__ == '__main__':
-	while True:
-		try:
-			print("Starting...")
-			crawl()
-		except Exception as e:
-			print("There was an issue.")
-			time.sleep(5)
-			continue
-=======
+
 while True:
 	try:
-		print("Starting...")
+		print("Starting my main method.")
 		crawl()
 	except Exception as e:
-		print("There was an issue.")
-		time.sleep(5)
+		with open(os.environ['USERPROFILE']+'/Documents/GitHub/HFBot/BotErrors.txt', 'a') as file:
+			file.write(str(datetime.now()) + " - " + str(e) + "\n")
+		print("Meh, got an error.")
 		continue
->>>>>>> 1c4e86a44e6b651835c87f29219e2e3b94b903de
+	except praw.exceptions.APIException as e:
+		print(e)
+		if e.error_type == "RATELIMIT":
+			print("Haven't quit, just sleeping on the job.")
+		# Remove this part once Karma is built up and rate limit is gone/diminished
+		time.sleep(600)
+		continue
